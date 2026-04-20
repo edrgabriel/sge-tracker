b2b83e3272333c5aecab9c816f4f2998a79820eb
edrgabriel <zer0cops100@gmail.com>
2026-04-20 02:27:43 -0300

    Deploy final: Migração Supabase e correções Vercel

---
+++ public/js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://nikrcdkgqqfmiigmaaya.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_13I7wz7owCKZXZhM8V79lQ_6inllWwF';
+
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
